5eb022c5cbaf50069a6d29b11ae2edbbe0f5ac00
Joanjo hm <mtz230396@gmail.com>
2019-07-10 13:28:24 -0500

    Add file login and folder routers

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from app import app
+from app.routes import *
+
+app.register_blueprint(routes)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3141592653589793238462643383279502884197169399'
-print("Init")
 
-from app.routers import app
+#if __name__ == '__main__':
+#    app.run(debug=True, host='0.0.0.0', port=3000)
+
+#from app.routers import app
